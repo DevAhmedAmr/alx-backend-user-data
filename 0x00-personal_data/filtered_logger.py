8f40feaa87da12e8b9539bcc77f48301b5abc8e3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-04 00:50:49 +0300

    added annotations (automated command)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,7 +15,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields) -> None:
+    def __init__(self, fields: List[str]) -> None:
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
@@ -56,9 +56,4 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(pattern, r"\1=" + redaction, message)
 
 
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord(
-    "my_logger", logging.INFO, None, None, message, None, None
-)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+function.__annotations__
