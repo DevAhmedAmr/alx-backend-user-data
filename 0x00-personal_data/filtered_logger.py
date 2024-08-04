f44404d7fd673ee17467b5690981f20949e244e6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-05 00:20:02 +0300

    2. Create logger (automated command)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -64,9 +64,12 @@ def get_logger() -> logging.Logger:
     Returns:
         logging.Logger: logger object
     """
-    logger = logging.Logger("user_data", logging.INFO)
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
     logger.addHandler(handler)
     return logger
 
