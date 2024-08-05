380f01038b8fbb1a39acad5b2d9f43f44df23a64
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-05 16:44:54 +0300

    hash_password func task5  (automated command)

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,6 +2,19 @@
 """
 Encrypting passwords
 """
+
 import bcrypt
 
+# example password
 
+
+def hash_password(password):
+    # converting password to array of bytes
+    bytes = password.encode('utf-8')
+
+    # generating the salt
+    salt = bcrypt.gensalt()
+
+    # Hashing the password
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
