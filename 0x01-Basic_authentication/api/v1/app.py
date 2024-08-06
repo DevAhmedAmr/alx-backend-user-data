78cf20ed61f9a0e07251f647ebf9d21725d4b2fe
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-07 00:06:11 +0300

    6. Basic auth  (automated command)

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,27 +20,31 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def Unauthorized_err(error) -> str:
-    """ Not found handler
+    """
+    Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_err(error) -> str:
-    """ Not found handler
+    """
+    Not found handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 def before_request():
-    """Checks if the user has authorized to access the request .
+    """
+    Checks if the user has authorized to access the request .
     """
 
     if not auth:
