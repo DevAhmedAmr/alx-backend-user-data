bb57a5c9fd021c499d6f6416f1fb5c7251308eb3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-06 22:37:54 +0300

    Refactor authorization check logic in Auth class
    Previously, the `require_auth` method in the Auth class always returned False, indicating that authentication was required for all paths. This commit modifies the method to properly handle path exclusion logic. Now, the method checks if the path is explicitly listed in the `excluded_paths` list or if a trailing slash version of the path is listed. If either condition is met, the method returns False, indicating that authentication is not required for the specified path. This change ensures more accurate handling of path-based authentication requirements.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,13 +18,20 @@ class Auth:
                 Returns:
                         bool: [description]
                 """
-        return False
+        if (not path) or not excluded_paths:
+            return True
+
+        if (path in excluded_paths or (path + "/") in excluded_paths):
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ the Authorization header for the request .
 
                 Args:
-                        request ([type], optional): [description]. Defaults to None.
+                        request ([type], optional): [description].
+                        Defaults to None.
 
                 Returns:
                         str: [description]
