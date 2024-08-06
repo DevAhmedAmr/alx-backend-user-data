5e3d36187badce45de7499cfd36d78f6b778a2fc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-06 21:16:00 +0300

    2. Error handler: Forbidden (automated command)

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,5 +31,12 @@ def unauthorized() -> str:
     """ GET /api/v1/unauthorized
           unauthorized
     """
-
     return abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+          forbidden
+    """
+    return abort(403)
