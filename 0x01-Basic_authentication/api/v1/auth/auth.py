463e0d561934f4a91125fa2cabd9be742bfa3d24
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-06 21:54:59 +0300

    3. Auth class  (automated command)

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3
-"""
-Auth class.
-
-	Returns:
-		[type]: [description]
-"""
+#!/usr/bin/env python3
+""" Auth class. """
 from flask import request
 from typing import List, TypeVar
 
