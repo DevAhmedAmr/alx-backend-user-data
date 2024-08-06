30685176368fc3b1dd2d7257761c687394311d02
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-06 21:48:01 +0300

    3. Auth class  (automated command)

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require authentication to be used for this client .
+
+                Args:
+                        path (str): [description]
+                        excluded_paths (List[str]): [description]
+
+                Returns:
+                        bool: [description]
+                """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ the Authorization header for the request .
+
+                Args:
+                        request ([type], optional): [description]. Defaults to None.
+
+                Returns:
+                        str: [description]
+                """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Return the current user s current user .
+
+                Returns:
+                        [type]: [description]
+                """
+        return None
