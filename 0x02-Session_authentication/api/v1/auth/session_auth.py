a4de1b379f4f970749f81c7f12e3ce70846a9a80
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-09 00:53:01 +0300

    Add Session Authentication and Test Cases
    Implement session authentication functionality in the SessionAuth class, including the creation of session IDs for user IDs using UUIDs. Update test cases to validate the session creation process, ensuring proper handling of different user ID inputs, including None, non-string types, and duplicate user IDs. This enhancement improves the authentication mechanism by introducing session management capabilities.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,9 +6,26 @@ from api.v1.auth.auth import Auth
 from typing import TypeVar
 from models.user import User
 import base64
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """A class method to create a session auth class .
     """
-    pass
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a new session id for user_id .
+
+                Args:
+                        user_id (str):. Defaults to None.
+
+                Returns:
+                        str: UUID
+                """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        uuid = uuid4()
+        SessionAuth.user_id_by_session_id[uuid] = user_id
+
+        return uuid
