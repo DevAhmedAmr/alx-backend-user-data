a48ea98d162ad6a49efa6ac3bfe860a8befd244d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-09 04:29:29 +0300

    Refactor session cookie retrieval for consistency
    Simplify the session cookie retrieval logic in the Auth class to directly return the session cookie if it exists in the request. This change enhances readability and consistency by removing an unnecessary conditional check. The method now directly accesses the session cookie using the environment variable `SESSION_NAME`, improving the overall clarity and efficiency of the code.

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -62,6 +62,5 @@ class Auth:
         if request is None:
             return request
 
-        if os.getenv("SESSION_NAME") in request.cookies:
-            return request.cookies.get(os.getenv("SESSION_NAME"))
+        return request.cookies.get(os.getenv("SESSION_NAME"))
 # SESSION_NAME
