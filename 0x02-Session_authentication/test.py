4663d86356828832fee350d0a7a7c93651c1ca07
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-10 04:23:25 +0300

    .. (automated command)

--- 0x02-Session_authentication/test.py
+++ 0x02-Session_authentication/test.py
@@ -2,11 +2,36 @@
 """ Check response
 """
 import requests
+import base64
 
 if __name__ == "__main__":
-    r = requests.get('http://0.0.0.0:50000/api/v1/users/me',
-                     cookies={'_my_session_id': "fake session ID"})
-    if r.status_code != 403:
+    user_email = "u5@hbtn.io"
+    user_pwd = "pwd5"
+
+    r = requests.post(
+        'http://0.0.0.0:50000/api/v1/auth_session/login',
+        data={
+            'email': user_email,
+            'password': user_pwd})
+    if r.status_code != 200:
         print("Wrong status code: {}".format(r.status_code))
         exit(1)
-    print("OK", end="")
+    if r.headers.get('content-type') != "application/json":
+        print("Wrong content type: {}".format(r.headers.get('content-type')))
+        exit(1)
+
+    try:
+        r_json = r.json()
+
+        r_user_email = r_json.get('email')
+        if r_user_email is None:
+            print("User is not return")
+            exit(1)
+
+        if r_user_email != user_email:
+            print("User returned is not the same: {}".format(r_json))
+            exit(1)
+
+        print("OK", end="")
+    except BaseException:
+        print("Error, not a JSON")
