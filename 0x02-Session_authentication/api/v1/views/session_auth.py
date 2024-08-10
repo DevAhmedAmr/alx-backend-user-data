bfec06336207217868fd427e0916a08932af939a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-10 04:06:45 +0300

    Add session authentication endpoints and ignore files for session authentication module
    - Created a new file `.vscode/easycode.ignore` to specify files and directories to be ignored in the session authentication module, including `node_modules/`, `dist/`, `vendor/`, and various file patterns.
    - Modified `api/v1/app.py` to remove unnecessary checks for the authorization header in the `before_request` function, streamlining the request handling process.
    - Updated `api/v1/views/__init__.py` to import session authentication views, enabling the integration of session-based authentication endpoints.
    - Added a new file `api/v1/views/session_auth.py` which contains the implementation of the session authentication login endpoint (`/auth_session/login`). This endpoint handles user login by validating the email and password, creating a session for the authenticated user, and setting the session ID in the response cookies.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" session_auth views
+"""
+# POST / auth_session/login
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.user import User
+from api.v1.auth.session_auth import SessionAuth
+import os
+
+
+@app_views.route('/auth_session/login',
+                 methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ GET /api/v1/login
+                login end point
+
+    Return:
+      - user object + session id in cookies
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email:
+        return {"error": "email missing"}, 400
+    if not password:
+        return {"error": "password missing"}, 400
+    user = User.search({"email": email})
+    if not user:
+        return {"error": "no user found for this email"}, 404
+    if user[0].password != password:
+        return {"error": "wrong password"}, 401
+
+    session_id = SessionAuth().create_session(user[0].id)
+
+    respone = jsonify(user[0].to_json())
+    respone.set_cookie(os.getenv("SESSION_NAME"), session_id)
+    return respone
