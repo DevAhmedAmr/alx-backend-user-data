cb11a9d016d4eda25d07fb0ec19ef4b0c466c1f0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-15 08:34:18 +0300

    Add .vscode/easycode.ignore and enhance user.py for user authentication service
    - Created a new .vscode/easycode.ignore file to specify patterns for files and directories that should be ignored by EasyCode in the user authentication service project. This includes common patterns for node modules, distribution files, vendor libraries, cache directories, minified files, test files, spec files, bundle files, and log files.
    - Enhanced the user.py file by adding a docstring to the User class, providing a clearer description of its purpose and functionality. The docstring explains that the class is designed to create a user-friendly SQLAlchemy class for user authentication purposes. Additionally, minor formatting and spacing adjustments were made to improve code readability and consistency.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+
+"""
+Create a user - friendly SQLAlchemy class .
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
 class User(Base):
+    """Class method to create a user .
+        """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
