ceacc28a1145c738981e3fadc664c91e3a8b9dab
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-15 10:39:44 +0300

    (automated command)

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -67,7 +67,8 @@ class DB:
         # user = self._session.scalar(query)
 
         query = self._session.query(User)
-        user = query.filter(tuple_(*attributes).in_([tuple(values)])).first()
+        user = query.filter(
+            *(getattr(User, key) == value for key, value in kwargs.items())).first()
 
         if not user:
             raise NoResultFound()
