df0ed282548c4596a0028bb12d557fd778896196
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-17 00:10:08 +0300

    Add password hashing functionality and update main script to use it
    Implemented a new function `_hash_password` in the `auth.py` module to securely hash user passwords using the bcrypt library. This function takes a plaintext password as input, encodes it to bytes, generates a salt, and then hashes the password using the salt. The resulting hashed password is returned as bytes.
    
    Updated the `main.py` script to import and demonstrate the use of the `_hash_password` function, replacing the previous example code that interacted with the database. This change showcases the new password hashing functionality, enhancing the security of user authentication processes in the application.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,3 +2,20 @@
 """
 Authentication module
 """
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    function to hash a password
+
+    Args:
+        password (str): pw to be hashed
+
+    Returns:
+        bytes: hashed password
+    """
+    bytes = password.encode('utf-8')
+    salt = gensalt()
+    hash = hashpw(bytes, salt)
+    return hash
