0614511af65f79dd2d12faa65901edbb8d42f0b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-08-17 07:20:15 +0300

    Add basic Flask app structure with home route
    This commit introduces a basic Flask application with a single route, "/", which
    returns a JSON message "Bienvenue" when accessed. The Flask app is initialized
    and configured to run on host "0.0.0.0" and port "5000". This setup serves as
    the foundation for further development and integration of user authentication
    features in the service.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,3 +2,19 @@
 """
 Flask app
 """
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """ home dir
+
+        Returns:
+                message
+        """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
